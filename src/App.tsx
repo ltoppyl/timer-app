e808633befd0ec4431a2bafdf79a247e39cd9f4b
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-02-02 12:38:39 +0900

    feat: 時間の選択 UI の作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
-import { Container, HStack } from "@chakra-ui/react";
+import {
+  Container,
+  HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import React from "react";
 
 import Time from "./components/Time";
 import StartStopButton from "./components/StartStopButton";
+// import SettingTimer from "./components/SettingTime";
+import { TimeIcon } from "@chakra-ui/icons";
+
 function App() {
   return (
     <Container maxW="container.sm">
       <Time />
       <HStack justifyContent={"center"}>
         <StartStopButton />
+        {/* <SettingTime /> */}
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            icon={<TimeIcon />}
+            color="black"
+            variant="outline"
+            _focus={{ boxShadow: "none" }}
+          />
+          <MenuList>
+            <MenuItem>1:00</MenuItem>
+            <MenuItem>3:00</MenuItem>
+            <MenuItem>5:00</MenuItem>
+            <MenuItem>10:00</MenuItem>
+          </MenuList>
+        </Menu>
       </HStack>
     </Container>
   );
