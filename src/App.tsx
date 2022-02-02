2182b1b2fc7361b7a6b9c7c468cf58cff9519c14
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-02-02 11:26:19 +0900

    feat: 時間をベタ書きでの記述

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Box, Container, Text } from "@chakra-ui/react";
 import React from "react";
 
 function App() {
-  return <h1>あ</h1>;
+  return (
+    <Container maxW="container.sm">
+      <Box pt={12} pb={9} bg="blue.100">
+        <Text fontFamily="Bold" fontSize={36} textAlign={"center"}>
+          10:00
+        </Text>
+      </Box>
+    </Container>
+  );
 }
 
 export default App;
