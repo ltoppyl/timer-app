6dddc1be69133242b999a75e9c1aeb8f91cb8ef1
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-02-03 15:04:55 +0900

    feat: 任意の時間選択からの手入力機能の実装

--- src/components/SettingTime.tsx
+++ src/components/SettingTime.tsx
@@ -1,10 +1,28 @@
-import React from "react";
-import { IconButton, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import React, { Dispatch, SetStateAction, useRef, useState } from "react";
+import {
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Input,
+  Button,
+} from "@chakra-ui/react";
 import { TimeIcon } from "@chakra-ui/icons";
 
-type Props = { setSettingTime: React.Dispatch<React.SetStateAction<number>> };
+type Props = { setSettingTime: Dispatch<SetStateAction<number>> };
 
 const SettingTime = ({ setSettingTime }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = useRef();
+
   return (
     <Menu>
       <MenuButton
@@ -45,11 +63,29 @@ const SettingTime = ({ setSettingTime }: Props) => {
         </MenuItem>
         <MenuItem
           onClick={() => {
-            setSettingTime(3600);
+            setIsOpen(true);
           }}
         >
           任意の時間
         </MenuItem>
+        <AlertDialog isOpen={isOpen} onClose={onClose} leastDestructiveRef={undefined}>
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Input Time
+              </AlertDialogHeader>
+              <AlertDialogBody>
+                <Input placeholder="Time" />
+              </AlertDialogBody>
+              <AlertDialogFooter>
+                <Button onClick={onClose}>Cancel</Button>
+                <Button colorScheme="blue" onClick={onClose} ml={3}>
+                  ENTER
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
       </MenuList>
     </Menu>
   );
