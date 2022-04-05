150122e76de69c327f279ae36fe28e7455883781
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-04-05 12:15:24 +0900

    refactor: 表示する文言の変更

--- src/pages/Countup.tsx
+++ src/pages/Countup.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Countup = () => {
-  return <p>CountUP!!</p>;
+  return <p> CountUP 機能は現在開発中</p>;
 };
 
 export default Countup;
--- src/pages/Time.tsx
+++ src/pages/Time.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Time = () => {
-  return <p>Time!!!</p>;
+  return <p> Time 機能は現在開発中</p>;
 };
 
 export default Time;
