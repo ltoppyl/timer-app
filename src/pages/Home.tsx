73869298e8f935edbb5aff94ccd5233ba3ed3913
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-04-05 12:31:02 +0900

    feat: 画面遷移の実装

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Button, GridItem, Link, VStack } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
-const Main = () => {
-  return <p>Main</p>;
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <VStack spacing={10}>
+        <GridItem w="100%" h="10" bg="white" />
+        <Button
+          w="100%"
+          colorScheme="gray"
+          onClick={() => {
+            navigate("/countdown");
+          }}
+        >
+          Countdown
+        </Button>
+        <Button
+          w="100%"
+          colorScheme="gray"
+          onClick={() => {
+            navigate("/countup");
+          }}
+        >
+          Countup
+        </Button>
+        <Button
+          w="100%"
+          colorScheme="gray"
+          onClick={() => {
+            navigate("/test");
+          }}
+        >
+          Time
+        </Button>
+      </VStack>
+    </>
+  );
 };
 
-export default Main;
+export default Home;
